6f6836aa0dff784f5d5d402a13c5fae2c95c434f
Tom Hoffman <tom.hoffman@gmail.com>
2023-11-27 17:56:38 -0500

    Update and rename lfsr.ts to main.ts
    Both shifts working.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 function shiftLeft() {
     let new0 = xor(room[bit(0)], xor(room[bit(6)],
         xor(room[bit(5)], room[bit(6)])))
-    room.push(new0)
-    room.removeAt(0)
+    room.unshift(new0)
+    room.removeAt(8)
 }
 function shiftRight() {
     let new7 = xor(room[bit(7)], xor(room[bit(5)],
@@ -11,6 +11,7 @@ function shiftRight() {
     room.removeAt(0)
 }
 let room: boolean[] = []
+let list = []
 room = [true, true, false, false, false, true, false, false]
 function bit(index: number) {
     return 7 - index
@@ -24,8 +25,5 @@ function xor(a: boolean, b: boolean) {
         return false
     }
 }
-shiftRight()
-console.log(room)
 forever(function () {
-
 })
